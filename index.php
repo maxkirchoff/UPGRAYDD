cd98f3b0f7e44860bcbaa082313a5c95e3b17e37
Max <max@shopigniter.com>
2012-09-06 17:43:31 -0700

    whoops

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ else
 {
     $process = curl_init($host . "/sfx");
     curl_setopt($process, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    curl_setopt($process, CURLOPT_USERPWD, "max@shopigniter.com:reedrichards32");
+    curl_setopt($process, CURLOPT_USERPWD, $username . ":" . $password);
     curl_setopt($process, CURLOPT_TIMEOUT, 30);
     curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
     $return = curl_exec($process);
